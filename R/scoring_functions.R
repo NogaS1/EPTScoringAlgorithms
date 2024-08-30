3113685b127891969bc8b5cc76b4d37f55616fc6
NogaS1 <73123787+NogaS1@users.noreply.github.com>
2024-08-30 20:26:16 +0300

    Update scoring_functions.R
    fix g_overall function

--- R/scoring_functions.R
+++ R/scoring_functions.R
@@ -140,55 +140,6 @@ calc_g_overall <- function(data){
   # Calc G score & fix col names
   overall <- my.Gscores(inID = data$sid, inDV = data$rt, inPair = data$prime_cat, inCond = data$cond, include = data$include)
   
-  tmp <- data.frame(sessionId=data$sid, dv=data$rt, pair=data$prime_cat, cond=data$cond, include=data$include)
-  # Comments in "quotes" from Nosek, Bar-Anan, Sriram, Greenwald (2013),
-  # "Understanding and using the brief implicit association test: I.
-  # recommended scoring procedures". Table 9. http://ssrn.com/abstract=2196002
-  
-  Gaussianranks <- function(x){
-    # It handles NA values by leaving them in the same place as found
-    y <- x[!is.na(x)]    
-    
-    # "1. Assign fractional ranks to N latencies. The longest latency will be
-    # assigned a value of 1.0 and the shortest will be assigned a value of 1/N.
-    # In the case of ties, ranks are averaged across tied values"
-    N <- length(y)
-    Fr <- rank(y)/N
-    
-    # "2. Subtract 1/2N from each fractional rank. Assuming untied values, the
-    # largest latency will now have a value of 1-1/2N or (2N-1)/2N.
-    # The 1/2N downward adjustment applies generally, even when tied values
-    # exist".
-    Fr <- Fr - 1/(2*N)
-    
-    # "3. For each of the N observations, compute the standard normal deviate
-    # (mean = 0 and standard deviation = 1) corresponding to the adjusted
-    # fractinal rank latency".
-    Gr <- scale(Fr)
-    
-    # Remove the attributes given by the scale() function
-    attr(Gr, "scaled:center") <- NULL
-    attr(Gr, "scaled:scale") <- NULL
-    x[!is.na(x)] <- Gr
-    return(x)
-  }
-  
-  # "4. G1 is the mean of the normal deviates in condition 1. G2 is the mean
-  # of the normal deviates in condition 2"
-  Mranks <- tmp %>% 
-    dplyr::group_by(sessionId) %>% # here do NOT group by blockcode
-    dplyr::mutate(Gr = Gaussianranks(dv)) %>% # compute gaussian ranks
-    dplyr::group_by(sessionId, pair, cond) %>% # here do group by blockcode
-    dplyr::summarize(Mean = mean(Gr, na.rm = TRUE)) # mean rank in each block
-  
-  Mranks <- reshape2::dcast(Mranks, sessionId + pair ~ cond, value.var = "Mean")
-  
-  # "5. G = G2-G1"
-  Mranks <- Mranks %>% dplyr::mutate(Gscore = incong - cong)
-  
-  # Put data in wide format
-  overall <- reshape2::dcast(Mranks, sessionId ~ pair, value.var = "Gscore")
-  
   # change col names
   colnames(overall)[1] <- "sid"
   names(overall)[names(overall) != "sid"] <- paste0("g_", names(overall)[names(overall) != "sid"])
